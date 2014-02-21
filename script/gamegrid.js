727225b96da43dd08792472da9639705f4481e62
Verna Koskinen <darth.kipsu@gmail.com>
2014-02-21 12:05:57 +0200

    add specs and function to create gameboard

--- script/gamegrid.js
+++ script/gamegrid.js
@@ -1,4 +1,15 @@
-function createTable() {
+function createGamegrid() {
 	var table = document.createElement('table');
+	table.id = 'gamegrid';
+	for (var i=1; i<9; i++) {
+		var tr = document.createElement('tr');
+		for (var j=1; j<9; j++) {
+			var td = document.createElement('td');
+			$(td).data('row', i);
+			$(td).data('column', j);
+			tr.appendChild(td);
+		};
+		table.appendChild(tr);
+	}
 	return table;
 };
--- specs/gamegrid.spec.js
+++ specs/gamegrid.spec.js
@@ -1,7 +1,23 @@
-describe("Gamegrid creation", function() {
-	it("creates a table", function() {
-		var result = createTable();
-		expect(result).toEqual(jasmine.any(HTMLElement));
-		expect(result.nodeName).toEqual('TABLE');
+describe("Gameboard creation", function() {
+	var result = createGamegrid();
+	it("creates a table with id gamegrid", function() {
+		expect(result).toEqual(jasmine.any(HTMLTableElement));
+		expect(result.id).toEqual('gamegrid');
+	});
+	it("creates eight rows in the table", function() {
+		var trs = result.children;
+		expect(trs.length).toEqual(8);
+		for (var i=0; i<trs.length; i++) {
+			expect(trs[i]).toEqual(jasmine.any(HTMLTableRowElement));
+		};
+	});
+	it("creates eight columns in the table with data-attributes presenting rows and columns", function() {
+		var tds = result.querySelectorAll('td');
+		expect(tds.length).toEqual(8 * 8);
+		for (var i=0; i<tds.length; i++) {
+			expect(tds[i]).toEqual(jasmine.any(HTMLTableCellElement));
+			expect($(tds[i]).data('row')).toEqual(Math.ceil((i + 1) / 8));
+			expect($(tds[i]).data('column')).toEqual((i + 1) - (Math.floor(i / 8)*8));
+		};
 	});
 });
