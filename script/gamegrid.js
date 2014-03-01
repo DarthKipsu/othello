2871902e0d2df775fb728ee3f0dc141458144f12
Verna Koskinen <darth.kipsu@gmail.com>
2014-03-01 21:30:39 +0200

    add 3d chip models

--- script/gamegrid.js
+++ script/gamegrid.js
@@ -6,6 +6,7 @@ function createGameboard() {
 	$('#game-wrapper').append(createChipSlot('player'), createChipSlot('opponent'), createGamegrid());
 	$('#player-chipslot').append(createChips('player'));
 	$('#opponent-chipslot').append(createChips('opponent'));
+	$('.chip').append(createChip3DLayers());
 };
 
 function createGamegrid() {
@@ -33,29 +34,32 @@ function addColumnsToGamegridRows(row, number) {
 }
 
 function createChipSlot (player) {
-	var slot = document.createElement('div'); 
+	var slot = document.createElement('div'); // slots for keeping unused chips
 	slot.id = player + "-chipslot";
 	return slot;
 }
-
-/*function createPlayerChipSlot() {
-	var playerSlot = document.createElement('div'); // div for player chips
-	playerSlot.id = "player-chipslot";
-	return playerSlot;
-}
-
-function createOpponentChipSlot() {
-	var opponentSlot = document.createElement('div'); // div for opponent chips
-	opponentSlot.id = "opponent-chipslot";
-	return opponentSlot;
-}*/
 
 function createChips(player) {
 	var chipsArray = [];
 	for (var i=0; i<32; i++) {
 		var chip = document.createElement('div'); // game chips * 32
-		chip.classList.add(player + '-unused');
+		chip.classList.add(player + '-unused', 'chip');
+		if (player == "opponent") {
+			chip.style.zIndex = (50 - i);
+		}
 		chipsArray.push(chip);
 	};
 	return chipsArray;
+};
+
+function createChip3DLayers() {
+	var chipSideArray = [];
+	for (var i = 0; i < 14; i++) {
+		var classArray = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+				  'nine', 'ten', 'eleven', 'twelve', 'top', 'bottom']
+		var chipSide = document.createElement('div'); // chip side pieces
+		chipSide.classList.add(classArray[i]);
+		chipSideArray.push(chipSide);
+	};
+	return chipSideArray;
 };
