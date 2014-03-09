25c598bc67cfd0108f1a73fe6d54f550aac5fdad
Verna Koskinen <darth.kipsu@gmail.com>
2014-03-09 13:03:15 +0200

    basic move a chip function, unfinished

--- script/move.js
+++ script/move.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-	placeFirstChips()
+	placeAChip()
 })
 
-function placeFirstChips() {
+function placeAChip() {
 
 	var chipCount = document.getElementById('chip')
 	
 	if (chipCount < 4) {
 		var playerChipslot = document.getElementById('player-chipslot')
-		var lastChild = playerChipslot.childElementCount
-		playerChipslot.removeChild(playerChipslot.childNodes[lastChild - 1])
+		var childCount = playerChipslot.childElementCount
+		playerChipslot.removeChild(playerChipslot.childNodes[childCount - 1])
+		var targetCell = $('#gamegrid tr:nth-child(4) td:nth-child(4)')
+		$(targetCell).append('<span class="black">X</span>')
 	}
 }
--- specs/move.spec.js
+++ specs/move.spec.js
@@ -1,23 +1,30 @@
-describe("move animation", function() {
-	it("make sure it's black or white players turn", function() {
-		expect(turn).toEqual("black"||"white")
+describe("turns are working", function() {
+	it("make sure game starts with black turn", function() {
+		expect(turn).toEqual("black")
 	})
 })
 
-describe("first move", function() {
+describe("move animation", function() {
 	
-	var gameWrapperDiv;
+	var gameWrapperDiv
 
 	beforeEach(function() {
-		$('body').empty();
-		gameWrapperDiv = document.createElement('div');
-		gameWrapperDiv.id = 'game-wrapper';
-		document.body.appendChild(gameWrapperDiv);
-	});
+		$('body').empty()
+		gameWrapperDiv = document.createElement('div')
+		gameWrapperDiv.id = 'game-wrapper'
+		document.body.appendChild(gameWrapperDiv)
+	})
 
 	it("removes one user chip from the chipslot", function() {
 		createGameboard()
-		placeFirstChips()
+		placeAChip()
 		expect($('#player-chipslot').children().length).toBe(31)
+	})
+
+	it("adds one user chip to the gameboard", function() {
+		createGameboard()
+		placeAChip()
+		blackChipsOnGameboard = $('.black')
+		expect(blackChipsOnGameboard.length).toBe(1)
 	})
 })
