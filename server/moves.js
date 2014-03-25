cd9d1df76bdf66681f73e95798a12dd01db87069
Verna Koskinen <darth.kipsu@gmail.com>
2014-03-25 21:50:17 +0200

    add first empty target position, unfinished

--- server/moves.js
+++ server/moves.js
@@ -8,3 +8,33 @@ gamegrid[3][3] = 'black'
 gamegrid[3][4] = 'white'
 gamegrid[4][3] = 'white'
 gamegrid[4][4] = 'black'
+
+function validPlacements(player) {
+	var opponent = player=='black'?'white':'black'
+
+	var opponentChips = []
+	for (var row=0; row<8; row++) {
+		for (var col=0; col<8; col++) {
+			var cell = gamegrid[row][col]
+			if (cell==opponent) {
+				opponentChips.push([row, col])
+			}
+		}
+	}
+
+	var validTargets = []
+	for (var i=0; i<opponentChips.length; i++) {
+		var row = opponentChips[i][0]
+		var col = opponentChips[i][1]-1
+		if (gamegrid[row][col]==undefined) {
+			validTargets.push([row, col])
+		}
+	}
+
+	console.log(gamegrid)
+	console.log(opponent)
+	console.log(opponentChips)
+	console.log(validTargets)
+}
+
+exports.validPlacements = validPlacements
---
+++ specs/serverspecs/moves.spec.js
@@ -0,0 +1,8 @@
+var moves = require('../../server/moves.js')
+
+describe("getting valid moves", function() {
+	it("should give valid chip placements to black player", function() {
+		var results = moves.validPlacements('black')
+		expect(results.length).toBe(4)
+	})
+})
